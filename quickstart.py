56ac0c931b7d426f0e26b9c27f4dc99d4d0c18bb
Devon A Loy <dloy3000@gmail.com>
2023-04-11 17:48:37 -0400

    Revoke.

--- quickstart.py
+++ quickstart.py
@@ -3,9 +3,9 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from spotipy.util import CLIENT_CREDS_ENV_VARS
 
-#alt method for creds
-os.environ["SPOTIPY_CLIENT_ID"] = "9189294878b349208c53d979e331073d"
-os.environ["SPOTIPY_CLIENT_SECRET"] = "838724677a564c1d8f6a3337d831646c"
+# alt method for creds
+os.environ["SPOTIPY_CLIENT_ID"] = ""
+os.environ["SPOTIPY_CLIENT_SECRET"] = ""
 os.environ["SPOTIPY_REDIRECT_URI"] = "http://localhost:8888/callback/"
 
 scope = "user-library-read, playlist-read-private, user-read-private, user-read-playback-position, user-read-recently-played, user-top-read"
